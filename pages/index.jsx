3d2c090c58ce1a504e6648df01b607c625ec19ba
Дениз Эрдоган <denis@scraper.local>
2022-07-12 20:01:16 +0300

    server side rendering

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,18 @@
 //   return Math.round(rand)
 // }
 
-import { useState } from 'react'
-
 export const getServerSideProps = async () => {
   const url = `${process.env.API_URL}?limit=100&offset=0}`
   const res = await fetch(url)
   const pokes = await res.json()
+
+  const { results } = pokes
+
+  const randomNum = Math.floor(Math.random() * (results.length - 1))
+  const randomPokemon = results[randomNum]
+  const resPoke = await fetch(randomPokemon.url)
+  const randomPokeObj = await resPoke.json()
+  console.log(randomPokeObj)
 
   return {
     props: {
@@ -17,35 +23,33 @@ export const getServerSideProps = async () => {
   }
 }
 
-function Poke({ pokes }) {
-  // const [randomPokemon, setRandomPokemon] = useState('')
-  const [randomPoke, setRandomPoke] = useState('Pokemon')
-  let pokeAll = Object.entries(pokes)
-  let pokeInfo = []
-  pokeAll[3][1].map(names => pokeInfo.push(names))
+function Poke() {
+  // const [randomPoke, setRandomPoke] = useState('Pokemon')
+  // let pokeAll = Object.entries(pokes)
 
-  let pokeNames = pokeInfo.map(a => a.name)
-
-  const randomNum = Math.floor(Math.random() * pokeNames.length)
+  // console.log(pokes)
+  // let pokeInfo = []
+  // pokeAll[3][1].map(names => pokeInfo.push(names))
+  //
+  // let pokeNames = pokeInfo.map(a => a.name)
 
   // Finding api url of generated Pokemon
-  const pokeObj = randomPoke
-    ? pokes['results'].find(item => item.name === randomPoke)
-    : ''
-  console.log(pokeObj['url'])
+  // const pokeObj = randomPoke
+  //   ? pokes['results'].find(item => item.name === randomPoke)
+  //   : ''
+  // console.log(pokeObj['url'])
 
+  // <button onClick={() => setRandomPoke(pokeNames[randomNum])}>
+  //   generate pokemon
+  // </button>
+  // <ul>
+  //   {pokeNames.map(pokeName => (
+  //     <li key={pokeName.id}>{pokeName}</li>
+  //   ))}
+  // </ul>
   return (
     <>
       <h1>Poke Names</h1>
-      <h1>{randomPoke}</h1>
-      <button onClick={() => setRandomPoke(pokeNames[randomNum])}>
-        generate pokemon
-      </button>
-      <ul>
-        {pokeNames.map(pokeName => (
-          <li key={pokeName.id}>{pokeName}</li>
-        ))}
-      </ul>
     </>
   )
 }
