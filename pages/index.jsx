b2d0856b742c52aa2c716f492ff22c74f5b0c284
Дениз Эрдоган <denis@scraper.local>
2022-07-12 15:43:13 +0300

    generate random pokemon when you press button and display it on screen also getting specific pokemon url

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,12 @@
-function Poke({ pokes }) {
-  let pokeAll = Object.entries(pokes)
-  let pokeInfo = []
-  pokeAll[3][1].map(names => pokeInfo.push(names))
+// const getRandomNum = (min, max) => {
+//   let rand = min - 0.5 + Math.random() * (max - min + 1)
+//   return Math.round(rand)
+// }
 
-  let pokeNames = pokeInfo.map(a => a.name)
-
-  return (
-    <>
-      <h1>Poke Names</h1>
-      <ul>
-        {pokeNames.map(pokeName => (
-          <li key={pokeName.id}>{pokeName}</li>
-        ))}
-      </ul>
-    </>
-  )
-}
-
-const getRandomNum = (min, max) => {
-  let rand = min - 0.5 + Math.random() * (max - min + 1)
-  return Math.round(rand)
-}
+import { useState } from 'react'
 
 export const getServerSideProps = async () => {
-  const url = `${process.env.API_URL}?limit=100&offset${getRandomNum(0, 1154)}`
+  const url = `${process.env.API_URL}?limit=100&offset=0}`
   const res = await fetch(url)
   const pokes = await res.json()
 
@@ -32,6 +15,39 @@ export const getServerSideProps = async () => {
       pokes,
     },
   }
+}
+
+function Poke({ pokes }) {
+  // const [randomPokemon, setRandomPokemon] = useState('')
+  const [randomPoke, setRandomPoke] = useState('Pokemon')
+  let pokeAll = Object.entries(pokes)
+  let pokeInfo = []
+  pokeAll[3][1].map(names => pokeInfo.push(names))
+
+  let pokeNames = pokeInfo.map(a => a.name)
+
+  const randomNum = Math.floor(Math.random() * pokeNames.length)
+
+  // Finding api url of generated Pokemon
+  const pokeObj = randomPoke
+    ? pokes['results'].find(item => item.name === randomPoke)
+    : ''
+  console.log(pokeObj['url'])
+
+  return (
+    <>
+      <h1>Poke Names</h1>
+      <h1>{randomPoke}</h1>
+      <button onClick={() => setRandomPoke(pokeNames[randomNum])}>
+        generate pokemon
+      </button>
+      <ul>
+        {pokeNames.map(pokeName => (
+          <li key={pokeName.id}>{pokeName}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
 export default Poke
