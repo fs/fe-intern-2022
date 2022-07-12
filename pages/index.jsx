4c5caa124cbbe7eace43dd81ccf3ee9d1cb47900
mat1x228 <boooo211@yandex.ru>
2022-07-12 11:21:13 +0300

    fetching poke

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,22 +1,33 @@
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-
-export default function Home() {
-  const [pokemons, setPokemon] = useState({})
-  const apiEndPoint = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
-
-  const getPokemon = async () => {
-    const { data: res } = await axios.get(apiEndPoint)
-    setPokemon(res)
-  }
-
-  useEffect(() => {
-    getPokemon()
-  }, [])
-
+function Poke({ pokes }) {
+  let pokeNames = Object.entries(pokes)
   return (
-    <>
-      <h1>pokemon count: {pokemons.count}</h1>
-    </>
+    <ul>
+      <h1>Poke count{pokes.count}</h1>
+      <h1>Poke Names</h1>
+      <ul>
+        {pokeNames[3][1].map(pokeName => (
+          <li key={pokeName.name}>{pokeName.name}</li>
+        ))}
+      </ul>
+    </ul>
   )
 }
+
+export async function getStaticProps() {
+  const res = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=100&offset=${randomNum(0, 1154)}`
+  )
+  const pokes = await res.json()
+
+  return {
+    props: {
+      pokes,
+    },
+  }
+}
+function randomNum(min, max) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1)
+  return Math.round(rand)
+}
+
+export default Poke
