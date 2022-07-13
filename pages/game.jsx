b4241b1b736b6d9794b891dc1863047c1e7830e3
Дениз Эрдоган <denis@scraper.local>
2022-07-14 00:17:03 +0300

    Added randomly generate pokemon with photo. Co-authored-by: @mat1x228

--- pages/game.jsx
+++ pages/game.jsx
@@ -1,27 +1,51 @@
+import Image from 'next/image'
+import { useState } from 'react'
+
 export const getServerSideProps = async () => {
   const url = `${process.env.API_URL}?limit=100&offset=0`
   const res = await fetch(url)
-  const pokes = await res.json()
+  const data = await res.json()
+  console.log(data)
 
-  const { results } = pokes
+  const { results: pokemons } = data
 
-  const randomNum = Math.floor(Math.random() * (results.length - 1))
-  const randomPokemon = results[randomNum]
+  const randomNum = Math.floor(Math.random() * (pokemons.length - 1))
+  const randomPokemon = pokemons[randomNum]
   const resPoke = await fetch(randomPokemon.url)
   const randomPokeObj = await resPoke.json()
 
   return {
     props: {
       randomPokeObj,
-      pokes,
+      data,
+      pokemons,
     },
   }
 }
 
-export default function Game() {
+export default function Game({ pokemons }) {
+  const [pokemon, setPokemon] = useState('')
+  const [photo, setPhoto] = useState('')
+  const randomNum = Math.floor(Math.random() * pokemons.length - 1)
+
+  function setObj(randomNum) {
+    const { name } = pokemons[randomNum]
+
+    setPhoto(
+      `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${
+        randomNum + 1
+      }.svg`
+    )
+
+    setPokemon(name)
+  }
+
   return (
     <div>
       <h1>Game Page</h1>
+      {photo && <Image width="300px" height="300px" alt="poke" src={photo} />}
+      <h2>{pokemon ? pokemon : 'Hit Generate to see Pokemon'}</h2>
+      <button onClick={() => setObj(randomNum)}>Generate</button>
     </div>
   )
 }
