8c100495a67b76d46a7b1d683622bba07a193986
nadezhdakharchuk <nadezhda.kharchuk@flatstack.com>
2022-07-14 16:00:49 +0300

    try ti fetch

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,25 +2,46 @@ import Image from 'next/image'
 import PokemonCard from 'components/PokemonCard'
 
 export const getServerSideProps = async () => {
+  const promises = [...Array(10).keys()].map(async () => {
+    const num = Math.floor(Math.random() * 900) + 1
+    return fetch(`${process.env.API_URL}/${num}`).then(response =>
+      response.json()
+    )
+  })
+
+  const results = await Promise.allSettled(promises)
+
+  const sucessValues = results.reduce((acc, item) => {
+    if (item.value) {
+      acc.push(item.value)
+    }
+    return acc
+  }, [])
+
   const collectingPokemons = num => {
     return fetch(`${process.env.API_URL}/${num}`)
       .then(res => res.json())
       .then(data => data)
   }
+
   const arrayPokemons = []
   for (let i = 1; i <= 10; i++) {
     const randomNum = Math.floor(Math.random() * 900) + 1
     const data = await collectingPokemons(randomNum)
     arrayPokemons.push(data)
   }
-  const arrayPokemonsMinimized = arrayPokemons.map(pokemon => {
-    return {
-      id: pokemon.id,
-      name: pokemon.name,
-      img: pokemon.sprites.other.dream_world.front_default,
-      types: pokemon.types,
+
+  const arrayPokemonsMinimized = sucessValues.map(
+    ({ id, name, sprites, types }) => {
+      return {
+        id,
+        name,
+        img: sprites?.other.dream_world.front_default,
+        types,
+      }
     }
-  })
+  )
+
   return {
     props: {
       arrayPokemons,
@@ -29,12 +50,20 @@ export const getServerSideProps = async () => {
   }
 }
 
-export function Poke({ arrayPokemonsMinimized, arrayPokemons }) {
-  console.log('Array down below', arrayPokemons)
-  console.log('Minimized array down below', arrayPokemonsMinimized)
+export function Poke({ arrayPokemonsMinimized, arrayPokemons, results }) {
+  // console.log('Array down below', arrayPokemons)
+  // console.log('Minimized array down below', arrayPokemonsMinimized)
   return (
     <>
       <h1>Pokemons</h1>
+      <div>
+        {arrayPokemonsMinimized.map(({ id, name: nameP, img, types }) => (
+          <div key={id}>
+            {nameP}
+            <img alt={nameP} src={img} />
+          </div>
+        ))}
+      </div>
     </>
   )
 }
