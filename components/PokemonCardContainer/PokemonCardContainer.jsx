6406b959f310760afd0c5cd6498da515e287e66e
Talentless-E <31585552+Talentless-E@users.noreply.github.com>
2022-07-18 17:55:12 +0300

    Correcting styles of components

--- components/PokemonCardContainer/PokemonCardContainer.jsx
+++ components/PokemonCardContainer/PokemonCardContainer.jsx
@@ -1,17 +1,11 @@
-import { Container } from 'styles/Container.styled'
+import { Container } from './styles'
 import MinimizedCard from 'components/MinimizedCard'
 import altImg from 'public/img/pokemon-img.svg'
 
 const PokemonCardContainer = ({ pokeMinimized, setPokemon, setActive }) => {
   return (
     <>
-      <Container
-        display="flex"
-        flexDirection="row"
-        gap="30px"
-        flexWrap="wrap"
-        justifyContent="center"
-      >
+      <Container>
         {pokeMinimized.map(({ id, name, img, types }) => {
           return (
             <MinimizedCard
---
+++ components/PokemonCardContainer/styles.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 30px;
+  flex-wrap: wrap;
+  justify-content: center;
+`
